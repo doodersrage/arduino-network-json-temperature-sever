7139c63c80cabe68344a9bd631ce544e3e4d5e8c
Robert McDowell <doodersrage@gmail.com>
2026-04-23 09:28:25 -0400

    AM2302 module setup
    Initial run with two AM2302 sensors

--- ext-tmp/ext-tmp.ino
+++ ext-tmp/ext-tmp.ino
@@ -1,9 +1,16 @@
 
+#include "DHT.h"
 #include <SPI.h>
 #include <Ethernet.h>
 #include <LiquidCrystal.h>
 #include <ArduinoJson.h>
 #include <avr/wdt.h>
+
+#define DHTPIN1 A4     // Digital pin connected to the DHT sensor
+#define DHTPIN2 A5     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22
+DHT dht1(DHTPIN1, DHTTYPE);
+DHT dht2(DHTPIN2, DHTTYPE);
 
 // initial
 char c = 0;           // received data
@@ -12,7 +19,7 @@ char command[2] = "\0";  // command
 // ethernet configuration
 byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDA, 0x02 };
 IPAddress arduinoIP(192, 168, 12, 111);
-IPAddress dnsIP(192, 168, 12, 10);
+IPAddress dnsIP(192, 168, 12, 8);
 IPAddress gatewayIP(192, 168, 12, 1);
 IPAddress subnetIP(255, 255, 255, 0);
 // start server on port 80, change to whatever port you would like
@@ -21,12 +28,6 @@ EthernetServer server(80);
 // LCD connections:
 LiquidCrystal lcd(8 ,9, A2, 5, 6, 3, A3);
 int backLight = 7;    // pin 13 will control the backlight
-
-//TMP36 Pin Variables
-int sensorPin = 4; //the analog pin the TMP36's Vout (sense) pin is connected to
-int sensorPin1 = 5; //the analog pin the TMP36's Vout (sense) pin is connected to
-                        //the resolution is 10 mV / degree centigrade with a
-                        //500 mV offset to allow for negative temperatures
  
 /*
  * setup() - this function runs once when you turn your Arduino on
@@ -36,6 +37,8 @@ void setup()
   // analoogue to digital pins
   pinMode(A2, INPUT_PULLUP);      
   pinMode(A3, INPUT_PULLUP);      
+  pinMode(A4, INPUT_PULLUP);      
+  pinMode(A5, INPUT_PULLUP);      
 
   // LCD panel 
   pinMode(backLight, OUTPUT);
@@ -54,24 +57,28 @@ void setup()
 
   wdt_enable(WDTO_8S); // Enable WDT with 8 seconds timeout
 
+  dht1.begin();
+  dht2.begin();
 }
  
 void loop()
 {
-  
-  //getting the voltage reading from the temperature sensor
-  int reading0 = analogRead(sensorPin);  
-  int reading1 = analogRead(sensorPin);  
-  // get temp average
-  int readingAVG = ((reading0 + reading1) / 2);
+  delay(2000); 
 
-  // converting that reading to voltage, for 3.3v arduino use 3.3
-  float voltage0 = reading0 * (5.0 / 1024.0);
-  float voltage1 = reading1 * (5.0 / 1024.0);
+  //get dht sensor data
+  float h1 = dht1.readHumidity();
+  float t1 = dht1.readTemperature(); // Celsius
+  float h2 = dht2.readHumidity();
+  float t2 = dht2.readTemperature();
+
+  if (isnan(h1) || isnan(t1)) {
+    Serial.println("Failed to read from DHT sensor!\n");
+    return;
+  }
 
   // now print out the temperature
-  float temperatureC0 = (voltage0 - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-  float temperatureC1 = (voltage1 - 0.5) * 100 ;  
+  float temperatureC0 = t1;  //converting from 10 mv per degree wit 500 mV offset
+  float temperatureC1 = t2;  
   float temperatureCAVG = ((temperatureC0 + temperatureC1) / 2);
                                                 //to degrees ((voltage - 500mV) times 100) 
   // now convert to Fahrenheit
@@ -79,14 +86,22 @@ void loop()
   float temperatureF1 = (temperatureC1 * 9.0 / 5.0) + 32.0;
   float temperatureFAVG = ((temperatureF0 + temperatureF1) / 2);
 
+  // now print out the temperature
+  float humidity0 = h1;  //converting from 10 mv per degree wit 500 mV offset
+  float humidity1 = h2;  
+  float humidityAVG = ((humidity0 + humidity1) / 2);
+
   // convert temp data to JSON
   JsonDocument doc;
   doc["temp"]["avg"]["c"] = temperatureCAVG;
   doc["temp"]["avg"]["f"] = temperatureFAVG;
+  doc["temp"]["avg"]["h"] = humidityAVG;
   doc["temp"]["0"]["c"] = temperatureC0;
   doc["temp"]["0"]["f"] = temperatureF0;
+  doc["temp"]["0"]["h"] = humidity0;
   doc["temp"]["1"]["c"] = temperatureC1;
   doc["temp"]["1"]["f"] = temperatureF1;
+  doc["temp"]["1"]["h"] = humidity1;
 
   // serialize json data
   char output[256];
@@ -142,15 +157,15 @@ void loop()
   }                                 //waiting a second
 
   // print temp data to LCD
-  String messageC = String(temperatureCAVG) + " degrees C";
+  String messageC = String(temperatureCAVG) + " C Hum " + String(humidityAVG);
   lcd.setCursor(0,0);
   lcd.print(messageC);    // change text to whatever you like. keep it clean!
   lcd.setCursor(0,1);           // set cursor to column 0, row 1
-  String messageF = String(temperatureFAVG) + " degrees F";
+  String messageF = String(temperatureFAVG) + " F " ;
   lcd.print(messageF);
 
   // delay for a seond then loop
-  delay(1000);  
+  //delay(1000);  
 
   wdt_reset();
 
